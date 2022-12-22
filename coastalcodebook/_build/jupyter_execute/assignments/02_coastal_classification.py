a6e80b41b1aafd857bf62b04656be38b5bf573f4
floriscalkoen <floris_calkoen@hotmail.com>
2022-12-23 07:36:32 +1100

    folder tree

---
+++ coastalcodebook/_build/jupyter_execute/assignments/02_coastal_classification.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Coastal classification 
+
+# In[1]:
+
+
+import os
+import pathlib
+import sys
+
+# Make coastmonitor library importable by appending root to path
+cwd = pathlib.Path().resolve()
+sys.path.append(str(cwd.parent))
+
+
+# In[2]:
+
+
+from utils.geometries import geo_bbox
+
