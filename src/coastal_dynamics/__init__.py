944dc8f15a4be5628b0812fe5f122ef415eb385f
floriscalkoen <floris_calkoen@hotmail.com>
2024-02-12 15:22:07 +0100

    bump v0.0.6

--- src/coastal_dynamics/__init__.py
+++ src/coastal_dynamics/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Floris Calkoen"""
 __email__ = "floris@calkoen.nl"
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 
 from .factory import QuestionFactory
 from .io import read_questions, write_questions
