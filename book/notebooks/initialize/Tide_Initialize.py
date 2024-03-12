390ed6f829ce3894dd175fd44a8a825926bc78b2
floriscalkoen <floris_calkoen@hotmail.com>
2024-03-12 12:50:02 +0100

    rm old files

--- book/notebooks/initialize/Tide_Initialize.py
+++ book/notebooks/initialize/Tide_Initialize.py
@@ -1,6 +1,4 @@
-# %% plot_tide_diagram -  from 2c
-
-
+# %% plot_tide_diagram - from 2c
 def plot_tide_diagram(angle_sun, angle_moon):
     fig, ax = plt.subplots()
 
@@ -726,5 +724,263 @@ def questions_3a2():
 
 
 ## Missing a condition that A!=B, ..
+# %% plot_2timeseries_with_interactive_controls - from 4b
 
-# %%
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import ipywidgets as widgets
+from warnings import filterwarnings
+from matplotlib.ticker import MaxNLocator, StrMethodFormatter
+
+
+def plot_2timeseries_with_interactive_controls(comps, dates, tide):
+    locs = ["Scheveningen", "Jakarta"]
+    all_comp = comps = [
+        "M2",
+        "S2",
+        "N2",
+        "K2",
+        "K1",
+        "O1",
+        "P1",
+        "Q1",
+        "M3",
+        "M4",
+        "M6",
+        "M8",
+        "S4",
+        "MN4",
+        "MS4",
+    ]
+    # Define a list of checkboxes for component selection and put them in one row
+    checkboxes = [
+        widgets.Checkbox(
+            value=(comp in ["M2"]),
+            description=comp,
+            layout=widgets.Layout(width="auto"),
+        )
+        for comp in comps
+    ]
+    checkbox_row = widgets.HBox(
+        checkboxes, layout=widgets.Layout(display="flex", flex_flow="row wrap")
+    )
+
+    # Plot with interactive slider and checkboxes
+    date_range_selector = widgets.SelectionRangeSlider(
+        options=[(date.strftime("%d/%m %Hh"), date) for date in dates],
+        index=(0, len(dates) - 1),
+        description="Dates",
+        orientation="horizontal",
+        layout={"width": "700px"},
+        continuous_update=False,
+        readout=True,
+    )
+
+    def plot_timeseries(date_range, **kwargs):
+        start_date, end_date = date_range
+
+        # Filter selected components
+        selected_components = [comp for comp, value in kwargs.items() if value]
+
+        # Plot selected components
+        fig, axes = plt.subplots(
+            nrows=3, ncols=2, figsize=(12, 7), sharex=True, constrained_layout=False
+        )
+
+        for comp in selected_components:
+            axes[0, 0].plot(
+                tide[locs[0]][comp.lower()][start_date:end_date],
+                label=comp,
+                linewidth=0.5,
+            )
+            axes[0, 1].plot(
+                tide[locs[1]][comp.lower()][start_date:end_date],
+                label=comp,
+                linewidth=0.5,
+            )
+        l = axes[0, 1].legend(
+            fontsize="small", loc="upper right", bbox_to_anchor=(1.2, 1)
+        )
+        for line in l.get_lines():
+            line.set_linewidth(3)
+        axes[0, 0].set_title(f"{locs[0]}", fontweight="bold")
+        axes[0, 1].set_title(f"{locs[1]}", fontweight="bold")
+
+        # Calculate and plot the sum
+        sum_values = [0] * len(locs)
+        for i, loc in enumerate(locs):
+            sum_values[i] = sum(
+                tide[loc][comp.lower()][start_date:end_date]
+                for comp in selected_components
+            )
+
+        axes[1, 0].plot(
+            sum_values[0].index,
+            sum_values[0],
+            color="darkblue",
+            label="Sum of selected components",
+            linewidth=0.5,
+        )
+        axes[1, 1].plot(
+            sum_values[1].index,
+            sum_values[1],
+            color="darkblue",
+            label="Sum of selected components",
+            linewidth=0.5,
+        )
+        l1 = axes[1, 1].legend(
+            fontsize="small", loc="upper right", bbox_to_anchor=(1, 1.2)
+        )
+        for line in l1.get_lines():
+            line.set_linewidth(3)
+
+        sum_allvalues = [0] * len(locs)
+        for i, loc in enumerate(locs):
+            sum_allvalues[i] = sum(
+                tide[loc][comp.lower()][start_date:end_date] for comp in all_comp
+            )
+        axes[2, 0].plot(
+            sum_allvalues[0].index,
+            sum_allvalues[0],
+            color="black",
+            label="Total tidal signal",
+            linewidth=0.5,
+        )
+        axes[2, 1].plot(
+            sum_allvalues[1].index,
+            sum_allvalues[1],
+            color="black",
+            label="Total tidal signal",
+            linewidth=0.5,
+        )
+        l2 = axes[2, 1].legend(
+            fontsize="small", loc="upper right", bbox_to_anchor=(1, 1.2)
+        )
+        for line in l2.get_lines():
+            line.set_linewidth(3)
+
+        # Set labels and legend
+        for ax in axes.flat:
+            ax.tick_params(axis="x", rotation=0)
+            ax.xaxis.set_major_formatter(mdates.DateFormatter("%d/%m\n%H:%Mh"))
+            ax.xaxis.set_major_locator(MaxNLocator(nbins=6))
+        fig.text(
+            0.05, 0.5, "Sea level [cm]", va="center", rotation="vertical", fontsize=14
+        )
+        fig.text(0.5, 0.05, "Time", ha="center", va="center", fontsize=14)
+        plt.show()
+
+    filterwarnings("ignore", category=UserWarning)
+
+    # Create an interactive widget with checkboxes
+    figure = widgets.interactive(
+        plot_timeseries,
+        date_range=date_range_selector,
+        **{checkbox.description: checkbox for checkbox in checkboxes},
+    )
+
+    # Create a new container for arranging controls
+    controls = widgets.VBox([figure.children[0], checkbox_row, figure.children[-1]])
+    controls.layout.height = "100%"
+    display(controls)
+
+
+# %% Questions in 4b_shallow_water_tides
+
+
+def questions_4b():
+    import numpy as np
+    import ipywidgets as widgets
+
+    options2 = ["M3", "M4", "M6", "M8", "S4"]
+
+    options1 = [
+        "M2",
+        "S2",
+        "N2",
+        "K2",  # semi-diurnal
+        "K1",
+        "O1",
+        "P1",
+        "Q1",  # diurnal
+        "M3",
+        "M4",
+        "M6",
+        "M8",
+        "S4",
+        "MN4",
+        "MS4",
+    ]  # short period (overtides)
+
+    options = [options1, options2]
+
+    Answer2 = ["M6"]
+    Answer1 = ["M4", "MS4", "MN4", "S4"]
+
+    answers = [Answer1, Answer2]
+
+    Q1 = " "
+    Q2 = " "
+
+    questions = [Q1, Q2]
+
+    def multiple_choice_question_body(Question, options, answer):
+        question_widget = widgets.Label(
+            value=Question, layout=widgets.Layout(width="500px")
+        )
+
+        # Use Checkbox widget for multiple-choice options
+        checkboxes = [widgets.Checkbox(description=opt, value=False) for opt in options]
+
+        submit_button = widgets.Button(
+            description="Check",
+        )
+
+        output_widget = widgets.HTML(
+            value="",
+            placeholder="",
+            disabled=False,
+            layout=widgets.Layout(width="500px", border="none"),
+            overflow="hidden",
+        )
+
+        VBox1 = widgets.VBox(
+            [question_widget] + checkboxes + [submit_button, output_widget]
+        )
+
+        display(VBox1)
+
+        def check_answers(button, answer=answer):
+            # Check if all checkboxes are selected
+            selected_options = [
+                checkbox.description for checkbox in checkboxes if checkbox.value
+            ]
+            if set(selected_options) == set(answer):
+                output_widget.value = '<span style="color: green;margin-left: 8px;">Correct! Well done.</span>'
+            else:
+                output_widget.value = '<span style="color: red;margin-left: 8px;">Incorrect, please try again.</span>'
+
+        submit_button.on_click(check_answers)
+
+    # make and display the questions
+
+    general_question1 = "1. Select components whose period is around 6 hours. Validate your choice with the spectral plot from notebook 2d_tidal_constituents (Tidal Amplitudes chapter)."
+
+    general_question2 = """2. Two important sources for non-linearity in the tidal propagation equations are bottom friction and continuity.
+    - Compare the periods of M2 and M3, M4, M6, and M8.
+    - Compare the periods of S2 and S4.
+    How were these higher harmonics generated? What is the main overtide that is generated as a consequence of the friction depending non-linearly on the tidal velocity?"""
+
+    general_question34 = """3. Select components M2 and M4.
+    - Are the two constituents in phase? What do you notice in the combined signal?
+    - Compare to Fig. 5.71 from the textbook. Determine which panels in Fig. 5.71 best match the signals from Scheveningen and Jakarta. Check if one of them is a combination of two panels.
+
+4. Select the main tidal constituents and compare the resulting combined signal (second plot) to the total tidal signal, which includes the overtides (third plot). What contribution do the overtides have to the total tidal signal?"""
+
+    print("\n\n\033[1m" + general_question1 + "\033[0m")
+    multiple_choice_question_body(questions[0], options1, answers[0])
+    print("\n\n\033[1m" + general_question2 + "\033[0m")
+    multiple_choice_question_body(questions[1], options2, answers[1])
+    print("\n\n\033[1m" + general_question34 + "\033[0m")
+
+    return None
